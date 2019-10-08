457d57a7ee5bc2ef1ebc6e369f5afedc6998a421
prasantib <prasanti.babu@gmail.com>
2019-10-08 17:05:55 -0400

    StringBoot_302

---
+++ src/main/java/com/example/demo/StringBoot302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot302Application.class, args);
+    }
+
+}
